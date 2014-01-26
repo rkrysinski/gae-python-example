a8d22b4b956fab679b8546815100bb48c17405b7
Roman Krysinski <rkrysinski@gmail.com>
2014-01-26 21:54:47 +0100

    Initial code

---
+++ guestbook/models.py
@@ -0,0 +1,11 @@
+from google.appengine.ext import db
+
+class Greeting(db.Model):
+    """Models an individual Guestbook entry with an author, content, and date."""
+    author = db.StringProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
+    
+    @classmethod
+    def get_key_from_name(cls, guestbook_name=None):
+        return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
---
+++ guestbook/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+from google.appengine.api import users
+
+from guestbook.models import Greeting
+
+import urllib
+
+def main_page(request):
+    guestbook_name = request.GET.get('guestbook_name', 'default_guestbook')
+    guestbook_key = Greeting.get_key_from_name(guestbook_name)
+    greetings_query = Greeting.all().ancestor(guestbook_key).order('-date')
+    greetings = greetings_query.fetch(10)
+    if users.get_current_user():
+        url = users.create_logout_url(request.get_full_path())
+        url_linktext = 'Logout'
+    else:
+        url = users.create_login_url(request.get_full_path())
+        url_linktext = 'Login'
+    template_values = {
+        'greetings': greetings,
+        'guestbook_name': guestbook_name,
+        'url': url,
+        'url_linktext': url_linktext,
+    }
+    return render_to_response('main_page.html', template_values, context_instance=RequestContext(request))
+
+def sign_post(request):
+    if request.method == 'POST':
+        guestbook_name = request.POST.get('guestbook_name')
+        guestbook_key = Greeting.get_key_from_name(guestbook_name)
+        greeting = Greeting(parent=guestbook_key)
+    
+        if users.get_current_user():
+            greeting.author = users.get_current_user().nickname()
+    
+        greeting.content = request.POST.get('content')
+        greeting.put()
+        return HttpResponseRedirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
+    return HttpResponseRedirect('/')
