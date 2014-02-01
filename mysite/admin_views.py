530d017dc864b16407def94c1d43f59c774df2f7
Roman Krysinski <rkrysinski@gmail.com>
2014-02-01 22:48:05 +0100

    admin page: CRUD for MenuItems

--- mysite/admin_views.py
+++ mysite/admin_views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from models import MenuItem, MenuItemForm
+from django.shortcuts import redirect
 
 def main(request):
-    template_values=None
+    template_values = MenuItem.all()
     return render_to_response('admin_page.html', template_values, context_instance=RequestContext(request))
+
+def menu_add(request):
+    if request.method == 'POST':
+        formset = MenuItemForm(request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect(menu_list)
+    else:
+        formset = MenuItemForm()
+    return render_to_response("add_menu.html", {        
+                                    "formset": formset,
+                              }, context_instance=RequestContext(request))    
+
+def menu_list(request):
+    return render_to_response("list_menu.html", {        
+                                    "menus": MenuItem.all(),
+                              }, context_instance=RequestContext(request))      
+    
+def menu_edit(request, key=None):
+    if not key:
+        return redirect(menu_list)
+    m = MenuItem.get(key)
+    if request.method == 'POST':
+        formset = MenuItemForm(request.POST, instance=m)
+        if formset.is_valid():
+            formset.save()
+            return redirect(menu_list)
+    else:
+        formset = MenuItemForm(instance=m)
+    return render_to_response("add_menu.html", {        
+                                    "formset": formset,
+                              }, context_instance=RequestContext(request))     
+    
+def menu_delete(request, key=None):
+    if not key:
+        return redirect(menu_list)
+    if request.POST.get('confirmation') == 'yes':
+        m = MenuItem.get(key)
+        if m: m.delete()
+    if request.POST.get('confirmation'):
+        return redirect(menu_list)
+    return render_to_response("confirmation.html", {}, context_instance=RequestContext(request))     
+    
--- mysite/models.py
+++ mysite/models.py
@@ -1,4 +1,7 @@
 from google.appengine.ext import db
+from google.appengine.ext.db import djangoforms
+from django.forms.widgets import CheckboxSelectMultiple
+from django.forms.fields import MultipleChoiceField
 
 #https://developers.google.com/appengine/articles/modeling
 #http://brizzled.clapper.org/blog/2008/08/07/writing-blogging-software-for-google-app-engine/
@@ -13,10 +16,36 @@ class Greeting(db.Model):
     @classmethod
     def get_key_from_name(cls, guestbook_name=None):
         return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
+    
+class Article(db.Model):
+    title         = db.StringProperty(required=True)
+    body          = db.TextProperty()
+    url_slug      = db.StringProperty()
+    displayCount  = db.IntegerProperty(default=0)
+    modified_when = db.DateTimeProperty(auto_now_add=True)
 
 class MenuItem(db.Model):
-    name     = db.StringProperty(required=True)
-    url_slug = db.StringProperty()
-    visible  = db.BooleanProperty(required=True, default=False)
-    sub_menu = db.ListProperty(db.Key)
+    name          = db.StringProperty(required=True)
+    visible       = db.BooleanProperty(default=False)
+    sub_menu      = db.ListProperty(db.Key, default=[])
+    article       = db.ReferenceProperty(Article)    
+
+class ListPropertyChoice(MultipleChoiceField):
+    def clean(self, value):
+        """ extending the clean method to work with GAE keys """
+        new_value = super(ListPropertyChoice, self).clean(value)
+        key_list = []
+        for k in new_value:
+            key_list.append(db.Model.get(k).key())
+        return key_list
     
+    def validate(self, value):
+        if value:
+            super(ListPropertyChoice, self).validate(value)
+    
+class MenuItemForm(djangoforms.ModelForm):
+    sub_menu = ListPropertyChoice(
+                        choices=[(m.key(), m.name) for m in MenuItem.all()]
+               )
+    class Meta:
+        model = MenuItem
